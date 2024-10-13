323dc43662bbab631f553e1a6ec837ed2cd399b3
G-B-Machado <machado.guilhermeb@gmail.com>
2024-10-13 01:00:01 -0300

    activity done

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,90 @@
 using System;
+using System.Collections.Generic;
+
+// Class to represent a comment
+class Comment
+{
+    public string CommenterName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
+
+// Class to represent a video
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    private List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return Comments;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Creating a list of videos
+        List<Video> videos = new List<Video>();
+
+        // Creating videos and adding comments
+        Video video1 = new Video("Learning C#", "CodeMaster", 600);
+        video1.AddComment(new Comment("John", "Great tutorial!"));
+        video1.AddComment(new Comment("Mary", "Very helpful, thank you!"));
+        video1.AddComment(new Comment("Peter", "Could you explain more about classes?"));
+        videos.Add(video1);
+
+        Video video2 = new Video("Chocolate Cake Recipe", "ChefExpert", 480);
+        video2.AddComment(new Comment("Anna", "It turned out delicious!"));
+        video2.AddComment(new Comment("Charles", "I'll try to make it this weekend"));
+        video2.AddComment(new Comment("Lucy", "What brand of chocolate do you use?"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Productivity Tips", "LifeHacker", 720);
+        video3.AddComment(new Comment("Frank", "These tips changed my routine!"));
+        video3.AddComment(new Comment("Beatrice", "Very good, but you missed talking about organization apps"));
+        video3.AddComment(new Comment("Ralph", "Loved it, I want to see more videos like this"));
+        videos.Add(video3);
+
+        // Displaying video information
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Duration: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Number of comments: {video.GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.CommenterName}: {comment.Text}");
+            }
+            Console.WriteLine();
+        }
     }
 }
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,9 +1,169 @@
 using System;
+using System.Collections.Generic;
+
+class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateProvince;
+    private string _country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateProvince = stateProvince;
+        _country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_streetAddress}\n{_city}, {_stateProvince}\n{_country}";
+    }
+}
+
+class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA();
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+}
+
+class Product
+{
+    private string _name;
+    private string _productId;
+    private decimal _price;
+    private int _quantity;
+
+    public Product(string name, string productId, decimal price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public decimal GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetProductId()
+    {
+        return _productId;
+    }
+}
+
+class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        decimal totalCost = 0;
+        foreach (var product in _products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+        totalCost += _customer.IsInUSA() ? 5 : 35;
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in _products)
+        {
+            label += $"{product.GetName()} (ID: {product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Criando endereços
+        Address usaAddress = new Address("123 Main St", "Anytown", "CA", "USA");
+        Address nonUsaAddress = new Address("456 Maple Rd", "Somewhere", "Ontario", "Canada");
+
+        // Criando clientes
+        Customer usaCustomer = new Customer("John Doe", usaAddress);
+        Customer nonUsaCustomer = new Customer("Jane Smith", nonUsaAddress);
+
+        // Criando produtos
+        Product product1 = new Product("Laptop", "TECH001", 999.99m, 1);
+        Product product2 = new Product("Mouse", "TECH002", 24.99m, 2);
+        Product product3 = new Product("Keyboard", "TECH003", 59.99m, 1);
+
+        // Criando pedidos
+        Order order1 = new Order(usaCustomer);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(nonUsaCustomer);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        // Exibindo informações dos pedidos
+        DisplayOrderInfo(order1);
+        Console.WriteLine();
+        DisplayOrderInfo(order2);
+    }
+
+    static void DisplayOrderInfo(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.CalculateTotalCost():F2}");
     }
 }
